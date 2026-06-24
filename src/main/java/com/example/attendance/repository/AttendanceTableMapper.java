2020fc49c28211e6a97c23b167c1c3e844e0e085
kato-isol <r-kato@i-sol.co.jp>
2026-06-24 11:33:10 +0900

    コミットb933ce9の追加

--- src/main/java/com/example/attendance/repository/AttendanceTableMapper.java
+++ src/main/java/com/example/attendance/repository/AttendanceTableMapper.java
@@ -1,10 +1,15 @@
 package com.example.attendance.repository;
 
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.example.attendance.entity.AttendanceData;
 
@@ -14,7 +19,7 @@ import com.example.attendance.entity.AttendanceData;
  * @version 2.0 2026-06-17 kato
  */
 @Mapper
-public interface WorkTableMapper {
+public interface AttendanceTableMapper {
 
 	/**
 	 * 指定した社員ID、範囲の開始日、範囲終了の次の日を条件にattendance_tableを検索し、<br>
@@ -33,4 +38,48 @@ public interface WorkTableMapper {
 			+ "AND work_day < #{workMonthEnd} ORDER BY shain_id DESC")
 	List<AttendanceData> selectAttendancetoAttendanceData(
 			int shainId, LocalDate workMonthStart, LocalDate workMonthEnd);
+
+	/**
+	 * 指定した社員ID、勤務日を条件にattendance_tableを検索し、<br>
+	 * 結果をAttendanceData型のOptionalに格納します
+	 * @param shainId 社員ID
+	 * @param workDay 検索対象の勤務日
+	 * @return 該当する情報を保持したAttendanceDataのOptional
+	 */
+	@Select("SELECT * FROM attendance_table WHERE shain_id = #{shainId} AND work_day = #{workDay}")
+	Optional<AttendanceData> selectAttendanceByWorkDay(int shainId, LocalDate workDay);
+
+	/**
+	 * 勤怠データをテーブルに新規登録（インサート）します。
+	 *
+	 * @param shainId   社員ID
+	 * @param workDay   出勤日
+	 * @param startTime 出勤時間（未入力や休みの場合は 00:00）
+	 * @param endTime   退勤時間（未入力の場合は 00:00）
+	 * @param note      備考
+	 */
+	@Insert("INSERT INTO attendance_table (shain_id,work_day,start_time,end_time,note) VALUES (#{shainId},#{workDay},#{startTime},#{endTime},#{note})")
+	void insertAttendanceData(
+			@Param("shainId") int shainId,
+			@Param("workDay") LocalDate workDay,
+			@Param("startTime") LocalTime startTime,
+			@Param("endTime") String endTime,
+			@Param("note") String note);
+	
+	/**
+	 *勤怠データを更新します。
+	 *
+	 * @param shainId   社員ID
+	 * @param workDay   出勤日
+	 * @param startTime 出勤時間（未入力や休みの場合は 00:00）
+	 * @param endTime   退勤時間（未入力の場合は 00:00）
+	 * @param note      備考
+	 */
+	@Update("UPDATE attendance_table SET start_time = #{startTime}, end_time = #{endTime}, note = #{note} WHERE shain_id = #{shainId} AND work_day = #{workDay} ")
+	void updateAttendanceData(
+			@Param("shainId") int shainId,
+			@Param("workDay") LocalDate workDay,
+			@Param("startTime") LocalTime startTime,
+			@Param("endTime") String endTime,
+			@Param("note") String note);
 }
